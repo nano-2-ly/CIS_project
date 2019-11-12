135eb2b00da3729f14c46853ddbea2a2eab2cec7
nano-2-ly <dong991207@unist.ac.kr>
2019-11-12 04:14:01 -0500

    8 step move update

--- arduino_serial.py
+++ arduino_serial.py
@@ -6,9 +6,18 @@ ser = serial.Serial('/dev/ttyACM0', 115200)
 s = [0,1]
 while True :
 	
-        ser.write(bytes('G0 X1\n'))
-        time.sleep(1)
-        ser.write(bytes('G0 X-1\n'))
-        time.sleep(1)
+	for i in range(4):
+            ser.write(bytes('G91\n'))	    
+            ser.write(bytes('G0 X2\n'))
+            time.sleep(1)
+            
+        ser.write(bytes('G0 Y3\n'))
+        
+        for j in range(4):
+            ser.write(bytes('G91\n'))    
+            ser.write(bytes('G0 X-2\n'))
+            time.sleep(1)
+            
+        ser.write(bytes('G0 Y-3\n'))
         
         
