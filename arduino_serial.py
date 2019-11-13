5d467dfda3c9a0258450c2528c0117b5765a7a0f
nano-2-ly <dong991207@unist.ac.kr>
2019-11-13 01:24:36 -0500

    XYZ control

--- arduino_serial.py
+++ arduino_serial.py
@@ -5,19 +5,21 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 115200)
 s = [0,1]
 while True :
-	
-	for i in range(4):
-            ser.write(bytes('G91\n'))	    
-            ser.write(bytes('G0 X2\n'))
-            time.sleep(1)
+    
+    for i in range(4):
+        ser.write(bytes('G91\n'))       
+        ser.write(bytes('G0 X2\n'))
+        time.sleep(0.1)
             
-        ser.write(bytes('G0 Y3\n'))
+    ser.write(bytes('G0 Y3\n'))
+    ser.write(bytes('G0 Z10\n'))
+    
+    for j in range(4):
+        ser.write(bytes('G91\n'))    
+        ser.write(bytes('G0 X-2\n'))
+        time.sleep(0.1)
         
-        for j in range(4):
-            ser.write(bytes('G91\n'))    
-            ser.write(bytes('G0 X-2\n'))
-            time.sleep(1)
-            
-        ser.write(bytes('G0 Y-3\n'))
-        
-        
+    ser.write(bytes('G0 Y-3\n'))
+    ser.write(bytes('G0 Z-10\n'))
+    
+    
